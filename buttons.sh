3752b178e228214140a9b82a74930d7627e6908c
jens <jens@lea.io>
2014-11-04 14:44:56 +0100

    adding startp scripts

---
+++ buttons.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env sh
+
+# optionally supply settings here
+python button_player.py --bouncetime 1000
---
+++ player.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env sh
+
+# optionally supply settings here
+python staalpiplayer.py
